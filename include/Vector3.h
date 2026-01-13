83a55d1fdab45d7b9dce7c44dcb2f3e17697235b
Saadan Naqvi <saadannaqvi@Saadans-MacBook-Pro.local>
2026-01-13 21:21:12 +0500

    Vector class creation

---
+++ include/Vector3.h
@@ -0,0 +1,14 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+class Vector3{
+    private:
+        double x;
+        double y;
+        double z;
+    public:
+        Vector3(double x, double y, double z);
+};
+
+
+#endif
