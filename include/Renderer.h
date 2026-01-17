b6b8e196293462804baee66f2640f90ba99b0624
Saadan Naqvi <saadannaqvi@Saadans-MacBook-Pro.local>
2026-01-17 22:15:23 +1030

    Added some more sphere functionaility need to add shaders before i can properly create a sphere

--- include/Renderer.h
+++ include/Renderer.h
@@ -10,11 +10,22 @@ class Renderer{
     private:
         int width;
         int height;
-        Vector3* camera;
+
+
+        const double PI = 3.14159265359;
+
+        unsigned int sphereVAO = 0;
+        unsigned int sphereVBO = 0;
+        unsigned int sphereEBO = 0;
+        unsigned int indexCount = 0;
+
+
     public:
         Renderer(int width, int height);
         void run();
         void processInput(GLFWwindow* window);
+        void generateSphere(double radius, int sectors, int stackCount, std::vector<Vector3>& vertices, std::vector<unsigned int>& indices);
+        void initSphere();
         void drawSphere();
 };
 
--- src/run/Renderer.cpp
+++ src/run/Renderer.cpp
@@ -38,12 +38,18 @@ void Renderer::run(){
         return;
     }
 
+    
+    glEnable(GL_DEPTH_TEST);
+    initSphere();
+
     // Render loop
     while(!glfwWindowShouldClose(window)){
         processInput(window);
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader.use();
+        drawSphere();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -60,6 +66,67 @@ void Renderer::processInput(GLFWwindow* window)
 
 
 
-void Renderer::drawSphere(){
+void Renderer::generateSphere(double radius, int sectors, int stackCount, std::vector<Vector3>& vertices, std::vector<unsigned int>& indices){
+    for(int i = 0; i<= stackCount; i++){
+        double stackAngle = PI / 2 - i * PI / stackCount;
+        double xy = radius * cosf(stackAngle);
+        double z = radius * sinf(stackAngle);
 
+        for(int j = 0; j <= sectors; ++j) {
+            double sectorAngle = j * 2 * PI / sectors;
+            Vector3 v(xy * cosf(sectorAngle), xy * sinf(sectorAngle), z);
+
+            vertices.push_back(v);
+        }
+    }
+
+    for (int i = 0; i < stackCount; i++) {
+        int k1 = i * (sectors + 1);
+        int k2 = k1 + sectors + 1;
+
+        for (int j = 0; j < sectors; j++, k1++, k2++) {
+            if (i != 0) {
+                indices.push_back(k1);
+                indices.push_back(k2);
+                indices.push_back(k1 + 1);
+            }
+            if (i != (stackCount - 1)) {
+                indices.push_back(k1 + 1);
+                indices.push_back(k2);
+                indices.push_back(k2 + 1);
+            }
+        }
+    }
+}
+
+void Renderer::drawSphere(){
+    glBindVertexArray(sphereVAO);
+    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+
+void Renderer::initSphere(){
+    std::vector<Vector3> vertices;
+    std::vector<unsigned int> indices;
+
+    generateSphere(1.0, 36, 18, vertices, indices);
+    indexCount = indices.size();
+
+    glGenVertexArrays(1, &sphereVAO);
+    glGenBuffers(1, &sphereVBO);
+    glGenBuffers(1, &sphereEBO);
+
+    glBindVertexArray(sphereVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vector3), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sphereEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vector3), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
 }
